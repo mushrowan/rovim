dcffc450d24877f9d010cea053553735d15efe40
rowan amber-jones <roarch@proton.me>
2025-09-08 14:00:22 +0100

    blink: add keybinds

--- lua/partials/plugins/completion.lua
+++ lua/partials/plugins/completion.lua
@@ -3,7 +3,12 @@ return {
 		"blink.cmp",
 		for_cat = "general.completion",
 		after = function(_)
-			require("blink.cmp").setup({})
+			require("blink.cmp").setup({
+				keymap = {
+					["<C-k>"] = { "select_prev", "fallback" },
+					["<C-j>"] = { "select_next", "fallback" },
+				},
+			})
 		end,
 	},
 }
