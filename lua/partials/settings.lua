073e1b7195c1e38ea2284765153e5430e9d85030
rowan amber-jones <roarch@proton.me>
2025-09-14 21:10:57 +0100

    settings: change neovide opacity

--- lua/partials/settings.lua
+++ lua/partials/settings.lua
@@ -50,20 +50,17 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 -- Neovide settings
-vim.g.neovide_normal_opacity = 0.9
+vim.g.neovide_normal_opacity = 0.95
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
 
 -- Neovide binds
 vim.g.neovide_scale_factor = 1.0
 local change_scale_factor = function(delta)
-  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
 vim.keymap.set("n", "<C-=>", function()
-  change_scale_factor(1.25)
+	change_scale_factor(1.25)
 end)
 vim.keymap.set("n", "<C-->", function()
-  change_scale_factor(1/1.25)
+	change_scale_factor(1 / 1.25)
 end)
-
-
-
