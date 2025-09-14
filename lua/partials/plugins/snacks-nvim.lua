c5edaf0a1166cfdb43865b8fcacf0c76627c5c89
rowan amber-jones <roarch@proton.me>
2025-09-14 20:14:03 +0100

    snacks: add lsp progress

--- lua/partials/plugins/snacks-nvim.lua
+++ lua/partials/plugins/snacks-nvim.lua
@@ -21,6 +21,51 @@ return {
 				terminal = { enabled = true },
 				lazygit = { enabled = true },
 			})
+			-- Notifier LSP progress from https://github.com/folke/snacks.nvim/blob/0ccf97c6e14149cdf1f03ca0186b39101174d166/docs/notifier.md
+			---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+			local progress = vim.defaulttable()
+			vim.api.nvim_create_autocmd("LspProgress", {
+				---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+				callback = function(ev)
+					local client = vim.lsp.get_client_by_id(ev.data.client_id)
+					local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+					if not client or type(value) ~= "table" then
+						return
+					end
+					local p = progress[client.id]
+
+					for i = 1, #p + 1 do
+						if i == #p + 1 or p[i].token == ev.data.params.token then
+							p[i] = {
+								token = ev.data.params.token,
+								msg = ("[%3d%%] %s%s"):format(
+									value.kind == "end" and 100 or value.percentage or 100,
+									value.title or "",
+									value.message and (" **%s**"):format(value.message) or ""
+								),
+								done = value.kind == "end",
+							}
+							break
+						end
+					end
+
+					local msg = {} ---@type string[]
+					progress[client.id] = vim.tbl_filter(function(v)
+						return table.insert(msg, v.msg) or not v.done
+					end, p)
+
+					local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+					vim.notify(table.concat(msg, "\n"), "info", {
+						id = "lsp_progress",
+						title = client.name,
+						opts = function(notif)
+							notif.icon = #progress[client.id] == 0 and " "
+								or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+						end,
+					})
+				end,
+			})
+
 			local terminal_keys = {
 				{
 					"<C-[>",
@@ -31,6 +76,9 @@ return {
 				},
 				-- Terminal raw mode binding here?
 			}
+			vim.keymap.set("n", "<leader>rf", function()
+				return Snacks.rename.rename_file()
+			end, { desc = "Rename File" })
 			vim.keymap.set("n", "<C-y>", function()
 				return Snacks.picker.resume()
 			end)
@@ -40,7 +88,7 @@ return {
 			vim.keymap.set("n", "<leader>sg", function()
 				return Snacks.picker.grep({ live = true })
 			end, { desc = "Live grep" })
-			vim.keymap.set("n", "<leader>rf", function()
+			vim.keymap.set("n", "<leader>sr", function()
 				return Snacks.picker.recent()
 			end, { desc = "Recent files" })
 			vim.keymap.set("n", "<leader>ss", function()
@@ -67,16 +115,13 @@ return {
 				return Snacks.explorer.open()
 			end, { desc = "Explorer" })
 			vim.keymap.set("n", "<leader>tt", function()
-				Snacks.terminal.toggle(
-          nil,
-					{
+				Snacks.terminal.toggle(nil, {
 					-- Prevents opening zellij
-						shell = "fish",
-						win = {
-							keys = terminal_keys,
-						},
-					}
-				)
+					win = {
+						position = "float",
+						keys = terminal_keys,
+					},
+				})
 			end, {
 				desc = "Toggle Terminal",
 			})
