a485c4eff0e5fbfd1576d922b0c9f69fb251df7b
rowan amber-jones <roarch@proton.me>
2025-09-22 14:33:00 +0100

    mappings: add ability to use escape in terminals

--- lua/partials/mappings.lua
+++ lua/partials/mappings.lua
@@ -1,8 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.keymap.set("t", "<C-[>", function()
+vim.keymap.set("t", "<C-[><C-[>", function()
 	vim.cmd("stopinsert")
 end, {
 	desc = "Exit terminal mode",
 })
-vim.keymap.set("n", "<C-q>", "<cmd>bd<CR>", { desc = "Delete current buffer" })
+vim.keymap.set("t", "<esc>", "<Nop>", { noremap = true })
+-- vim.keymap.set("n", "<C-q>", "<cmd>bd<CR>", { desc = "Delete current buffer" })
