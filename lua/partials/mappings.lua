ef00d87e15cc82e8b14790c1918cbb69e02f0485
rowan amber-jones <roarch@proton.me>
2025-11-09 20:11:23 +0000

    add mappings for window switching

--- lua/partials/mappings.lua
+++ lua/partials/mappings.lua
@@ -6,5 +6,15 @@ end, {
 	desc = "Exit terminal mode",
 })
 -- vim.keymap.set("t", "<esc>", "<Nop>", { noremap = true })
-vim.keymap.set("n", "<C-q>", "<cmd>bd<CR>", { desc = "Delete current buffer", silent = true })
 vim.keymap.set("i", "<C-CR>", "<Esc>jA", { desc = "Go to end of next line", silent = true })
+
+require("partials.utils").map_normal_all({
+	{ "<C-q>", "<cmd>bd<CR>", "Delete current buffer" },
+	{ "<A-j>", "<C-w>j", "Focus window below" },
+	{ "<A-k>", "<C-w>k", "Focus window above" },
+	{ "<A-h>", "<C-w>h", "Focus window left" },
+	{ "<A-l>", "<C-w>l", "Focus window right" },
+	{ "<A-v>", "<C-w>v", "Split vertically" },
+	{ "<A-s>", "<C-w>v", "Split horizonally" },
+	{ "<A-=>", "<C-w>=", "Resize splits to be equal" },
+})
