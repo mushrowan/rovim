4ff523bc0393771e4628ae64ceb665716f3c70b9
rowan amber-jones <roarch@proton.me>
2026-01-10 20:24:50 +0000

    flake: switch to unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -265,7 +265,7 @@
     };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-direnv-nvim.url = "github:mushrowan/nixpkgs/direnv-nvim";
     nixCats.url = "github:BirdeeHub/nixCats-nvim";
 
